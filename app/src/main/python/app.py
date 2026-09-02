cf35a5efceec90a8089678d3cef061a021c26fe3
Simple <157860608+vishala5000@users.noreply.github.com>
2026-09-02 15:35:49 +0530

    Update app.py

--- app/src/main/python/app.py
+++ app/src/main/python/app.py
@@ -1,1 +1,13 @@
+import os
 
+def main():
+    print("Python is running inside Android")
+
+    home = os.environ["HOME"]
+
+    output = os.path.join(home, "output.txt")
+
+    with open(output, "w") as f:
+        f.write("Python APK working!")
+
+    return output
